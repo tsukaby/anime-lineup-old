b8cd59fe181e5b18028e8d4436ecf48d57cc05c5
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2014-02-20 21:21:45 +0900

    レイアウトが崩れていたため、フォーマット。

--- app/js/app.js
+++ app/js/app.js
@@ -3,17 +3,16 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-            return $routeProvider.when("/", {
-              controller: 'animeListController',
-              templateUrl: 'partials/anime_list.html'
-            }).when("/:year/:season", {
-              controller: 'animeListController',
-              templateUrl: 'partials/anime_list.html'
-            }).when("/season_calendar", {
-              controller: 'seasonController',
-              templateUrl: 'partials/season_list.html'
-            });
-          }]).run(function($route) {
+]).config(['$routeProvider', function($routeProvider) {
+    return $routeProvider.when("/", {
+      controller: 'animeListController',
+      templateUrl: 'partials/anime_list.html'
+    }).when("/:year/:season", {
+      controller: 'animeListController',
+      templateUrl: 'partials/anime_list.html'
+    }).when("/season_calendar", {
+      controller: 'seasonController',
+      templateUrl: 'partials/season_list.html'
+    });
+  }]).run(function($route) {
 });
--- app/js/services.js
+++ app/js/services.js
@@ -3,91 +3,87 @@
 // シーズンに関する機能を提供するユーティリティ
 angular.module('myApp').factory("seasonService", function() {
   return {
-  // 現在日時から現在のシーズンを求める関数
+    // 現在日時から現在のシーズンを求める関数
     currentSeason: function() {
-    var now = new Date();
-    var year = now.getFullYear();
-    var season;
-    if (1 <= now.getMonth() && now.getMonth() <= 3) {
-      season = SeasonEnum.winter;
-    } else if (4 <= now.getMonth() && now.getMonth() <= 6) {
-      season = SeasonEnum.spring;
-    } else if (7 <= now.getMonth() && now.getMonth() <= 9) {
-      season = SeasonEnum.summer;
-    } else if (10 <= now.getMonth() && now.getMonth() <= 12) {
-      season = SeasonEnum.autumn;
-    }
+      var now = new Date();
+      var year = now.getFullYear();
+      var season;
+      if (1 <= now.getMonth() && now.getMonth() <= 3) {
+        season = SeasonEnum.winter;
+      } else if (4 <= now.getMonth() && now.getMonth() <= 6) {
+        season = SeasonEnum.spring;
+      } else if (7 <= now.getMonth() && now.getMonth() <= 9) {
+        season = SeasonEnum.summer;
+      } else if (10 <= now.getMonth() && now.getMonth() <= 12) {
+        season = SeasonEnum.autumn;
+      }
 
-    return {
-      year: year,
-      season: season
-    };
+      return {
+        year: year,
+        season: season
+      };
     },
-
-  //引数から次のシーズンを求める関数
+    //引数から次のシーズンを求める関数
     nextSeason: function(year, season) {
-    switch (season) {
-      case "winter":
-        return {
-          year: year,
-          season: "spring"
-        };
-        break;
-      case "spring":
-        return {
-          year: year,
-          season: "summer"
-        };
-        break;
-      case "summer":
-        return {
-          year: year,
-          season: "autumn"
-        };
-        break;
-      case "autumn":
-        // アニメは冬シーズンから始まるため、秋の次は年度が増える
-        return {
-          year: Number(year) + 1,
-          season: "winter"
-        };
-        break;
-    }
+      switch (season) {
+        case "winter":
+          return {
+            year: year,
+            season: "spring"
+          };
+          break;
+        case "spring":
+          return {
+            year: year,
+            season: "summer"
+          };
+          break;
+        case "summer":
+          return {
+            year: year,
+            season: "autumn"
+          };
+          break;
+        case "autumn":
+          // アニメは冬シーズンから始まるため、秋の次は年度が増える
+          return {
+            year: Number(year) + 1,
+            season: "winter"
+          };
+          break;
+      }
     },
-
-
-  //引数から前のシーズンを求める関数
+    //引数から前のシーズンを求める関数
     previousSeason: function(year, season) {
-    switch (season) {
-      case "winter":
-        // アニメは冬シーズンから始まるため、冬の前は年度が減る
-        return {
-          year: Number(year) - 1,
-          season: "autumn"
-        };
-        break;
-      case "spring":
-        return {
-          year: year,
-          season: "winter"
-        };
-        break;
-      case "summer":
-        return {
-          year: year,
-          season: "spring"
-        };
-        break;
-      case "autumn":
-        return {
-          year: year,
-          season: "summer"
-        };
-        break;
-    }
+      switch (season) {
+        case "winter":
+          // アニメは冬シーズンから始まるため、冬の前は年度が減る
+          return {
+            year: Number(year) - 1,
+            season: "autumn"
+          };
+          break;
+        case "spring":
+          return {
+            year: year,
+            season: "winter"
+          };
+          break;
+        case "summer":
+          return {
+            year: year,
+            season: "spring"
+          };
+          break;
+        case "autumn":
+          return {
+            year: year,
+            season: "summer"
+          };
+          break;
+      }
     },
-
-  //季節名を日本語に変える関数
+    //季節名を日本語に変える関数
     toJapaneseForSeason: function(season) {
       var seasons = {"spring": "春", "summer": "夏", "autumn": "秋", "winter": "冬"};
       return seasons[season];
