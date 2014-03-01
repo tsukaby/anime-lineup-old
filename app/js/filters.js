582ac401eeb36f1d8367be4437094f797d47ed04
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2014-03-01 09:11:25 +0900

    #2 シーズンのバッジにリンクと色を付加。

---
+++ app/js/filters.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('myApp').filter("seasonColor", function() {
+  return function(input) {
+    if(input === "winter"){
+      return "#5141D9";
+    }else if(input === "spring"){
+      return "#B2F63D";
+    }else if(input === "summer"){
+      return "#FF4540";
+    }else if(input === "autumn"){
+      return "#FFAC40";
+    }else{
+      return "#000000";
+    }
+  };
+});
+
